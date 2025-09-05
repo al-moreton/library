557ab4fb513f827ad1dea2b3fb5d05ff65609a20
al-moreton <moreton.alexander@gmail.com>
2025-09-05 19:32:51 +0100

    Table layout added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const addBookButton = document.querySelector('.add-book-button');
 const addBookModal = document.querySelector('dialog');
 const closeBookForm = document.querySelector('.close-book-form');
 const addBookForm = document.querySelector('.add-book-form');
+const changeLayoutButton = document.querySelector('.change-layout');
 const deleteBookButtons = document.getElementsByClassName('delete-book');
 const toggleReadButons = document.getElementsByClassName('toggle-read');
 
@@ -11,12 +12,18 @@ const author = document.querySelector('#book-author');
 const pages = document.querySelector('#book-pages');
 const read = document.querySelector('#book-read');
 
+let displayMode = 'grid';
+
 addBookButton.addEventListener('click', () => {
     addBookModal.showModal();
 })
 
 closeBookForm.addEventListener('click', () => {
     closeModal();
+})
+
+changeLayoutButton.addEventListener('click', () => {
+    toggleLayout();
 })
 
 addBookForm.addEventListener('submit', (e) => {
@@ -31,14 +38,14 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = Number(pages);
-    this.read = false;
+    this.read = false;  
 }
 
 Book.prototype.isRead = function () {
     return (this.read) ? 'Has been read' : 'Not read yet';
 }
 
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
     if (this.read) {
         this.read = false;
     } else if (!this.read) {
@@ -53,6 +60,26 @@ function addBookToLibrary(title, author, pages) {
 }
 
 function displayAllBooks() {
+    if (displayMode === 'table'){
+        const table = document.createElement('table');
+        table.classList.add('table');
+        const content = `
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Pages</th>
+                    <th scope="col">Read?</th>
+                    <th scope="col">&nbsp;</th>
+                    <th scope="col">&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody class="table-body"></tbody>
+        `;
+        table.innerHTML = content;
+        bookList.appendChild(table);
+    }
+
     myLibrary.forEach((book) => {
         displayBook(book);
     })
@@ -73,7 +100,18 @@ function displayAllBooks() {
     }
 }
 
-function displayBook(book) {
+function toggleLayout() {
+    if (displayMode === 'grid') {
+        displayMode = 'table';
+    } else if (displayMode == 'table') {
+        displayMode = 'grid';
+    }
+    refreshBookList();
+}
+
+function gridLayout(book) {
+    bookList.classList.add('book-list-grid');
+    bookList.classList.remove('book-list-table');
     const bookCard = document.createElement('article');
     bookCard.classList.add('book-card');
     bookCard.dataset.id = book.id;
@@ -91,6 +129,31 @@ function displayBook(book) {
     `;
     bookCard.innerHTML = content;
     bookList.appendChild(bookCard);
+}
+
+function tableLayout(book) {
+    bookList.classList.remove('book-list-grid');
+    bookList.classList.add('book-list-table');
+    const tableRow = document.createElement('tr');
+    const table = document.querySelector('.table-body');
+    const content = `
+        <th class="row-header"scope="row">${book.title}</th>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read}</td>
+        <td><button type="button" class="delete-book" data-id="${book.id}">Delete</button></td>
+        <td><button type="button" class="toggle-read" data-id="${book.id}">Toggle read</button></<td>
+    `;
+    tableRow.innerHTML = content;
+    table.appendChild(tableRow);
+}
+
+function displayBook(book) {
+    if (displayMode === 'grid') {
+        gridLayout(book);
+    } else if (displayMode == 'table') {
+        tableLayout(book);
+    }
 }
 
 function closeModal() {
@@ -128,5 +191,18 @@ addBookToLibrary('Wasp Factory', 'Iain M Banks', 435);
 addBookToLibrary('Dune', 'Frank Herbert', 765);
 addBookToLibrary('The Magus', 'John Fowles', 378);
 addBookToLibrary('Prophet Song', 'Rachel Morris', 378);
+addBookToLibrary('Homo Deus', 'Rachel Morris', 768);
 
 displayAllBooks();
+
+// Display total number of books, and total read
+// Change delete icon to bin
+// Button to change display to table instead of grid
+// Add favourite icon
+// Ability to sort books
+// Add read icon to top right, if clicked changes status
+// Add edit button
+// Add image URL
+// Add local storage
+// Add date added to object
+// Add rating
